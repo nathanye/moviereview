f8b2efb5ad34ed5cd6f01e6e5f9db896901da6f8
nathanye <khalil54781@gmail.com>
2017-03-24 18:01:07 +0800

    implement #review edit & destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,28 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:movie_id])
+    @review = Review.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to account_reviews_path, notice: "更新成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to account_reviews_path, alert:"删除成功！"
+  end
+
   private
 
   def review_params
