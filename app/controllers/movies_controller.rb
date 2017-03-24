7f4487956682fb4bb777460f4c192c9736e07ed0
nathanye <khalil54781@gmail.com>
2017-03-24 08:58:47 +0800

    use before_action to find_movie_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
+
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_movie_and_check_permission ,only: [:edit, :update, :destroy]
+
   def index
     @movies = Movie.all
   end
@@ -9,11 +12,6 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "你的操作不被允许！"
-    end
   end
 
   def new
@@ -32,12 +30,6 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "你的操作不被允许。"
-    end
-
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "更新成功"
     else
@@ -46,17 +38,19 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
-
-    if current_user != @movie.user
-      redirect_to root_path, alert: "你的操作不被允许。"
-    end
-
     @movie.destroy
     redirect_to movies_path, alert: "删除成功"
   end
 
   private
+
+  def find_movie_and_check_permission
+    @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "你的操作不被允许。"
+    end
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :description)
